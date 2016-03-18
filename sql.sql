eaaf9b159f3bc0eb654a29fdac08b53d1277be9b
Grace-Choi <grace.chanyang.choi@gmail.com>
2016-03-18 12:06:15 +0900

    add spl

--- sql.sql
+++ sql.sql
@@ -1,3 +1,6 @@
+DROP TEMPORARY TABLE IF EXISTS revenue_per_film;
+CREATE TEMPORARY TABLE revenue_per_film
+
 SELECT
   f.film_id,
   f.title,
@@ -10,4 +13,19 @@ WHERE
   r.inventory_id = i.inventory_id
   AND i.film_id = f.film_id
 GROUP BY 1
+  ;
+
+SELECT 
+  a.actor_id,
+  CONCAT (a.first_name, " ", a.last_name) name,
+  SUM(rpf.total_revenue) total_revenue_per_actor
+FROM 
+  revenue_per_film rpf,
+  film_actor fa,
+  actor a
+WHERE 
+  rpf.film_id = fa.film_id
+  AND a.actor_id = fa.actor_id
+GROUP BY 1
+ORDER BY total_revenue_per_actor
   ;
