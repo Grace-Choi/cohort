ca32285384300022b4d41b87cc73fef9b75930f2
Grace-Choi <grace.chanyang.choi@gmail.com>
2016-03-21 11:19:01 +0900

    update

--- sql3.sql
+++ sql3.sql
@@ -1,4 +1,13 @@
+DROP TEMPORARY TABLE IF EXISTS first_rental;
+CREATE TEMPORARY TABLE first_rental
 SELECT
-FROM
-WHERE
+  r.customer_id,
+  MIN(r.rental_date) first_time
+FROM 
+  rental r
+GROUP BY 1
+;
+
+SELECT *
+FROM first_rental
 ;
