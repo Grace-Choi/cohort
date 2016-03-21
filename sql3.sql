38bdc75f035fe8f80a4d60fc458a077fc0f57f34
Grace-Choi <grace.chanyang.choi@gmail.com>
2016-03-21 11:32:58 +0900

    update2 sql3.sql

--- sql3.sql
+++ sql3.sql
@@ -1,5 +1,8 @@
+#유저별 매출 수
+
 DROP TEMPORARY TABLE IF EXISTS first_rental;
 CREATE TEMPORARY TABLE first_rental
+
 SELECT
   r.customer_id,
   MIN(r.rental_date) first_time
@@ -10,4 +13,21 @@ GROUP BY 1
 
 SELECT *
 FROM first_rental
+;
+
+#cohort에 포함된 유저 수 
+
+DROP TEMPORARY TABLE IF EXISTS cohort_size;
+CREATE TEMPORARY TABLE cohort_size
+
+SELECT
+  LEFT(first_time, 7) Month,
+  COUNT(*) Num
+FROM
+  first_rental
+GROUP BY 1
+;
+
+SELECT *
+FROM cohort_size
 ;
